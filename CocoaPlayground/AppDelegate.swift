e51deb481098ce65380ad6e9f44550c80297d842
jiansoung <Jian.Soung@outlook.com>
2018-09-24 15:16:39 +0800

    Initial Commit

---
+++ CocoaPlayground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaPlayground
+//
+//  Created by songjian on 2018/9/24.
+//  Copyright © 2018 Jian Soung's Studio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaPlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaPlayground
+//
+//  Created by songjian on 2018/9/24.
+//  Copyright © 2018 Jian Soung's Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
