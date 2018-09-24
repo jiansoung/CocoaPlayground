51dfa325380528f58b69d7925a1e41c42ac56fcf
jiansoung <Jian.Soung@outlook.com>
2018-09-24 15:27:43 +0800

    Add `MainWindowController.swift` & `MainWindowController.xib` files.

---
+++ CocoaPlayground/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  CocoaPlayground
+//
+//  Created by songjian on 2018/9/24.
+//  Copyright © 2018 Jian Soung's Studio. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
